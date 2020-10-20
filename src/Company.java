8ce6b862a70979f02b520da2ebdac37bb01ea11f
Sebastian Różyczka <rozyczka.sebastian@gmail.com>
2020-10-20 19:43:50 +0200

    commit: fix

--- src/Company.java
+++ src/Company.java
@@ -1,12 +1,9 @@
 public class Company {
     private Employee[] employees = new Employee[3];
-    int nextIndex = 0;
+    private int nextIndex = 0;
 
-    public String get(int index) {
-        return "Pracownik o indexie: " + index + " to: " +
-                employees[index - 1].getFirstName()
-                + " " + employees[index - 1].getLastName() + ". Jego wypłata to: "
-                + employees[index - 1].getSalary() + " zł.";
+    public Employee get(int index) {
+        return employees[index];
     }
 
     public void add(Employee employee) {
--- src/Employee.java
+++ src/Employee.java
@@ -3,6 +3,12 @@ public class Employee {
     private String lastName;
     private double salary;
 
+    public Employee(String firstName, String lastName, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -24,12 +30,6 @@ public class Employee {
     }
 
     public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public Employee(String firstName, String lastName, double salary) {
-        this.firstName = firstName;
-        this.lastName = lastName;
         this.salary = salary;
     }
 }
